2b263d9806041ef8f23cbc23f773d83d9b30b9bb
Tomasz Izdebski <tomizdebski@gmail.com>
2024-11-23 18:28:19 +0100

    features/auth-is-working-jwt-working

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
+
 @Module({
+  imports: [],
   providers: [UsersService],
   controllers: [UsersController]
 })
--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { hash } from 'bcrypt';
 
