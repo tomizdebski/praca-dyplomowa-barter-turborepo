91653b8afcefecf2cd2b91fc59f997100d6df117
Tomasz Izdebski <tomaszizdebski@Mac-mini-Tomasz.local>
2024-12-10 14:09:01 +0100

    add-index

--- apps/api/src/quiz/quiz.service.ts
+++ apps/api/src/quiz/quiz.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { QUIZ } from './db_json';
 
 @Injectable()
 export class QuizService {
   getQuestionsByTopic(topic: string, numQuestions: number) {
+    const quiz = QUIZ[topic.toUpperCase()];
+    if (!quiz) {
+      return `No questions found for topic ${topic}`;
+    }
+    console.log(quiz);
     return `Getting ${numQuestions} questions for topic ${topic}`;
   }
 }
